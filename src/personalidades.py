c1c9ec3caae407a39aa967e04d65e5182b208e74
Gabriel <santos.gabriel@ufca.aluno.edu.br>
2025-09-12 14:44:05 -0300

    Melhorando a função responder() em ChatBot

--- src/personalidades.py
+++ src/personalidades.py
@@ -1,27 +1,24 @@
+from utils.checagem_de_texto import Texto as t
+
 class Personalidade:
-        
-    def selecionar_personalidade() -> str:
-        opcoes_formal = ('1', 'formal', 'n.u.t.r.i. bot')
-        opcoes_engraçado = ('2', 'engraçado', 'engracado', 'engraçada', 'engracada', 'nutrilove')
+
+    def selecionar_personalidade(resposta_usuario: str) -> str|bool:
+        """
+        Se o texto passado correspnder a uma personalidade, retorna a personalidade(str).
+        Senão, retorna Falso(bool).
+        """
+        opcoes_formal = ('1','formal', 'nutri bot')
+        opcoes_engracado = ('2', 'engraçado', 'nutrilove')
         opcoes_rude = ('3', 'rude', 'chief')
 
-        while True:
-            # ----- TRECHO COM INPUT / PRINT -----
-            personalidade = input(
-                '\nCom qual personagem você deseja conversar? '
-                '\n( 1 ) N.U.T.R.I Bot (formal)'
-                '\n( 2 ) NutriLove (engraçada)'
-                '\n( 3 ) Chief (rude)\n'
-            ).strip().lower()
+        if t.detectar_comando(resposta_usuario, opcoes_formal) in opcoes_formal:
+            return 'formal'
 
-            if personalidade in opcoes_formal:
-                return 'formal'
+        elif t.detectar_comando(resposta_usuario, opcoes_engracado) in opcoes_engracado:
+            return 'engraçado'
 
-            elif personalidade in opcoes_engraçado:
-                return 'engraçado'
-
-            elif personalidade in opcoes_rude:
-                return 'rude'
+        elif t.detectar_comando(resposta_usuario, opcoes_rude) in opcoes_rude:
+            return 'rude'
                 
-            else:
-                print('Essa personalidade não está disponível. Tente novamente.')
+        else:
+            return False
--- utils/checagem_de_texto.py
+++ utils/checagem_de_texto.py
@@ -1,25 +1,23 @@
 import string
 from difflib import SequenceMatcher
+from utils.tokens import Tokens
 
 class Texto:
-    
+        
+    @staticmethod
     def similaridade(msg_usuario: str, comando: str) -> float:
+        """
+        Checa a similariedade entre duas Strings e retorna um número
+        entre 0 e 1. quanto mais similar, mais próximo de 1.
+        """
         return SequenceMatcher(None, msg_usuario.lower(), comando.lower()).ratio()
     
+    @staticmethod
     def limpar_texto(texto: str) -> str:
+        """
+        Retorna um texto com sem pontuação e com mais palavras núcleo.
+        """
         texto = texto.lower().strip()
-
-        # Palavras que não carregam muito significado
-        tokens_ignorados = {
-            "poderia", "gostaria", "posso", "devo", "deveria"
-            "eu", "me", "você", "voce", "vc",
-            "quero", "são", "ser", "estar", "ter",
-            "as", "os", "de", "do", "dos", "da", "das",
-            "um", "uns", "uma", "umas",
-            "em", "na", "nas", "no", "nos",
-            "que", "para", "pr", "por", "com", "se"
-        }
-        
         # Remover pontuação
         texto = texto.translate(str.maketrans("", "", string.punctuation))
 
@@ -29,15 +27,24 @@ class Texto:
         # 2) palavras de tamanho 1 (len(p) == 1)
         palavras_filtradas = []
         for p in palavras:
-            if p not in tokens_ignorados and len(p) > 1:
+            if p not in Tokens.ignorados and len(p) > 1:
                 palavras_filtradas.append(p)
 
         return " ".join(palavras_filtradas)
     
-    def detectar_comando(txt_usuario, base_dados) -> str:
+    @staticmethod
+    def detectar_comando(txt_usuario: str, base_dados) -> str:
+        """
+        Recebe uma String e um conjunto de Strings. Se a String passada for
+        similar sufiente, a função retorna uma correspondete do conjuto.
+        Senão, retorna a própria String recebida.
+        """
         for pergunta in base_dados:
 
-            if Texto.similaridade(
+            if Texto.limpar_texto(txt_usuario) == '':
+                return txt_usuario
+
+            elif Texto.similaridade(
                 Texto.limpar_texto(txt_usuario), 
                 Texto.limpar_texto(pergunta)
             ) > 0.85: # 85% de similariedade
